93976454ebf1da5b81dfc129dfd9ff84bb6373ce
Han <morrs456@gmail.com>
2022-11-22 10:17:37 +1300

    NavBar

---
+++ src/Components/Add.js
@@ -0,0 +1,10 @@
+const Add = () => {
+  return (
+    <div className="add">
+      <h2>Write</h2>
+      <p>Create a new post.</p>
+    </div>
+  );
+};
+
+export default Add;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Name</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/add">New Blog</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
