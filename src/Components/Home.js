0e2692ae51ee19347e10f71143e5b20982880c19
Han <morrs456@gmail.com>
2022-11-24 20:23:58 +1300

    Homepage & fetching data from JSON server

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+import "./Home.css";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Oops something went wrong");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setBlogs(null);
+        setError(err.message);
+      });
+  }, []);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>This is homepage.</p>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="This is homepage." />}
     </div>
   );
 };
