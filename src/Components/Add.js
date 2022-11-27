63e307c9c3c68310220174a8a74fb0ae56963370
Han <morrs456@gmail.com>
2022-11-28 01:51:51 +1300

    Add new posts

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,8 +1,55 @@
+import { useState } from "react";
+import "./Add.css";
+import { useNavigate } from "react-router-dom";
+
 const Add = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setIsPending(false);
+      navigate("/");
+    });
+  };
+
   return (
     <div className="add">
       <h2>Write</h2>
-      <p>Create a new post.</p>
+      <form onSubmit={handleSubmit}>
+        <label>Title: </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Body: </label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Author: </label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button>Adding Blog</button>}
+      </form>
     </div>
   );
 };
