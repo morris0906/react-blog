6b5a668e3a9730f5f484315859bba2cedecccc21
Han <morrs456@gmail.com>
2023-03-14 02:02:37 +1300

    Confirming password done

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,41 +1,106 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LogIn.css";
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [error, setError] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const onInputChage = (e) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    validateInput(e);
+  };
+
+  const validateInput = (e) => {
+    let { name, value } = e.target;
+    setError((prev) => {
+      const stateObj = { ...prev, [name]: "" };
+
+      switch (name) {
+        case "username":
+          if (!value) {
+            stateObj[name] = "Please enter username.";
+          }
+          break;
+
+        case "password":
+          if (!value) {
+            stateObj[name] = "Please enter Password.";
+          } else if (input.confirmPassword && value !== input.confirmPassword) {
+            stateObj["confirmPassword"] =
+              "Password and re-entered passwords do not match.";
+          } else {
+            stateObj["confirmPassword"] = input.confirmPassword
+              ? ""
+              : error.confirmPassword;
+          }
+          break;
+
+        case "confirmPassword":
+          if (!value) {
+            stateObj[name] = "Please re-enter password.";
+          } else if (input.password && value !== input.password) {
+            stateObj[name] = "Password and re-entered passwords do not match.";
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      return stateObj;
+    });
+  };
+
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmailAddress] = useState("");
-  const [passwordVal, setPasswordVal] = useState("");
+  // const [passwordVal, setPasswordVal] = useState("");
   const [isPending, setIsPending] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    if (passwordVal !== password) {
-      document.getElementById("passwordCheck").innerHTML =
-        "Your passwords do not match!";
-      alert("Check passwords");
-    } else {
-      document.getElementById("passwordCheck").innerHTML = "";
+    e.preventDefault();
+    let username = input.username;
+    let password = input.password;
+    const userInfo = { username, password, firstName, lastName, email };
+    setIsPending(true);
 
-      e.preventDefault();
-      const userInfo = { username, password, firstName, lastName, email };
-      setIsPending(true);
-
-      fetch("https://localhost:5000/addUser", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(userInfo),
-      }).then(() => {
-        setIsPending(false);
-        navigate("/signin");
-      });
-    }
+    fetch("https://localhost:5000/addUser", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(userInfo),
+    }).then(() => {
+      setIsPending(false);
+      navigate("/signin");
+    });
   };
+
+  // const confirmPassword = () => {
+  //   if (passwordVal !== password) {
+  //     document.getElementById("passwordCheck").innerHTML =
+  //       "Your passwords do not match!";
+  //   } else {
+  //     document.getElementById("passwordCheck").innerHTML = "";
+  //   }
+  // };
 
   return (
     <div className="Register">
@@ -65,25 +130,33 @@ const Register = () => {
         <label>Username: </label>
         <input
           type="text"
+          name="username"
           required
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={input.username}
+          onChange={onInputChage}
         />
         <label>Password: </label>
         <input
           type="password"
+          name="password"
           required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={input.password}
+          onChange={onInputChage}
+          onBlur={(e) => validateInput(e)}
         />
+        {error.password && <span className="err">{error.password}</span>}
         <label>Re-enter Password: </label>
         <input
           type="password"
+          name="confirmPassword"
           required
-          value={passwordVal}
-          onChange={(e) => setPasswordVal(e.target.value)}
+          value={input.confirmPassword}
+          onChange={onInputChage}
+          onBlur={(e) => validateInput(e)}
         />
-        <p id="passwordCheck"></p>
+        {error.confirmPassword && (
+          <span className="err">{error.confirmPassword}</span>
+        )}
         {!isPending && <button>Submit</button>}
         {isPending && <button>Please wait</button>}
       </form>
